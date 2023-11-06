dc65a3d58f60a6aff6e283e0da4e391790ffd853
huniiverse <huniivers@gmail.com>
2023-11-06 15:10:37 +0900

    v0

---
+++ week11/readfile.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	err = f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
