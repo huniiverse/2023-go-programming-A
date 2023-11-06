f96ac1988d83e7c8067b4cde69794e6b6081ed29
huniiverse <huniivers@gmail.com>
2023-11-06 14:13:32 +0900

    v1

--- week11/main.go
+++ week11/main.go
@@ -8,6 +8,10 @@ func main() {
 	// primes[1] = 3
 	// primes[2] = 5
 	// fmt.Println(primes, primes[1])
-	var primes [3]int = [3]int{2, 3, 5}
+	//var primes [3]int = [3]int{2, 3, 5} // primes 배열을 배열 리터럴로 초기화
+	primes := [3]int{2, 3, 5} // 단축 연산자
 	fmt.Println(primes, primes[1])
+
+	test := [5]bool{true, true, true}
+	fmt.Println(test[3]) // boolean 타입의 제로값, false
 }
