596899b2d79cdb17551ef86dee6798f8830e3ed2
huniversal <hunivers0523@naver.com>
2023-11-13 14:59:42 +0900

    v5

--- week12/main.go
+++ week12/main.go
@@ -3,33 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	// var s []int
-	// s = make([]int, 5)
-	// s := make([]int, 5)
-	// 단축 연산자, make 함수를 이용해 슬라이스 생성 후 메모리 할당, 제로 값 적용
+	a := []string{"a", "b", "c", "d"}
+	as := a[:2]
+	as[1] = "z"
+	fmt.Println(a)
+	fmt.Println(as)
 
-	s := []int{0, 0, 0, 0, 0, 0}
-	// 슬라이스 리터럴을 이용해 슬라이스 생성 및 메모리 할당, 초기화 진행
-
-	s[4] = 99
-	s[2] = 91
-
-	for _, value := range s {
-		fmt.Println(value)
-	}
-
-	copyS := s[1:4]
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
-
-	test := [3]string{"inha", "go", "student"} // 배열 리터럴을 이용해서 test 배열 생성
-	// testS := test[0:4]	// invalid argument: index 4 out of bounds [0:4]
-	testS := test[:2] // testS := test[0:2]
-	testS2 := test[1:]
-	testS2[0] = "Python"
-	fmt.Println(testS, len(testS))
-	fmt.Println(testS2)
-	fmt.Println(test)
-
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(bs)
 }
+
+// func main() {
+// 	// var s []int
+// 	// s = make([]int, 5)
+// 	// s := make([]int, 5)
+// 	// 단축 연산자, make 함수를 이용해 슬라이스 생성 후 메모리 할당, 제로 값 적용
+
+// 	s := []int{0, 0, 0, 0, 0, 0}
+// 	// 슬라이스 리터럴을 이용해 슬라이스 생성 및 메모리 할당, 초기화 진행
+
+// 	s[4] = 99
+// 	s[2] = 91
+
+// 	for _, value := range s {
+// 		fmt.Println(value)
+// 	}
+
+// 	copyS := s[1:4]
+// 	for _, value := range copyS {
+// 		fmt.Println(value)
+// 	}
+
+// 	test := [3]string{"inha", "go", "student"} // 배열 리터럴을 이용해서 test 배열 생성
+// 	// testS := test[0:4]	// invalid argument: index 4 out of bounds [0:4]
+// 	testS := test[:2] // testS := test[0:2]
+// 	testS2 := test[1:]
+// 	testS2[0] = "Python"
+// 	fmt.Println(testS, len(testS))
+// 	fmt.Println(testS2)
+// 	fmt.Println(test)
+
+// }
